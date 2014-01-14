f570be0955bc33b3fb1456fcb71d168dbd44cd18
d-sepulveda <d@splvd.com>
2014-01-14 15:35:56 -0300

    Update chrome.sh

--- chrome.sh
+++ chrome.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 wget --delete-after http://localhost/$1/descuentos.php
+wget --delete-after http://localhost/$1/descuentos.html
+wget --delete-after http://localhost/$1/eventos.html
+wget --delete-after http://localhost/$1/cine.html
 export DISPLAY=:0.0
 xinput set-prop "FREE INTERACTIVE TECHNOLOGY " "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1
 openbox & google-chrome http://localhost/$1 --kiosk --incognito --proxy-server=127.0.0.1:3128 --disk-cache-dir=/dev/null
