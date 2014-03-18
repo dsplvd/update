2b27c88d056d69afd49f557e0283c6b83a57dc37
d-sepulveda <d@splvd.com>
2014-03-18 17:28:40 -0300

    Update updatejs.sh

--- updatejs.sh
+++ updatejs.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-wget http://www.mallplaza.cl/xml/descuentos.php?siteid=mallplaza-$1 --output-document=descuentos.xml && sleep 10 && wget http://www.mallplaza.cl/xml/cine.php?siteid=mallplaza-$1 --output-document=cine.xml && sleep 10 && wget http://www.mallplaza.cl/xml/eventos.php?siteid=mallplaza-$1 --output-document=eventos.xml && sleep 10 && wget http://www.mallplaza.cl/xml/queestapasando.php?siteid=mallplaza-$1 --output-document=pasando.xml && sleep 10 && wget http://www.mallplaza.cl/xml/tiendas.php?siteid=mallplaza-$1 --output-document=tiendas.xml
+sudo service polipo stop
+sudo polipo -x
+wget http://www.mallplaza.cl/xml/descuentos.php?siteid=mallplaza-$1 --output-document=descuentos.xml && sleep 10 && wget http://www.mallplaza.cl/xml/cine.php?siteid=mallplaza-$1 --output-document=cine.xml && sleep 10 && wget http://www.mallplaza.cl/xml/eventos.php?siteid=mallplaza-$1 --output-document=eventos.xml && sleep 10 && wget http://www.mallplaza.cl/xml/queestapasando.php?siteid=mallplaza-$1 --output-document=pasando.xml
 sleep 5
+ls pasando.xml >/dev/null || exit
 /home/ddmallplaza/update/./xidel --proxy=127.0.0.1:8123 descuentos.xml -f //imagen
-sleep 10
+sleep 5
 /home/ddmallplaza/update/./xidel --proxy=127.0.0.1:8123 cine.xml -f //afiche
-sleep 10
+sleep 5
 /home/ddmallplaza/update/./xidel --proxy=127.0.0.1:8123 eventos.xml -f //afiche
-sleep 10
+sleep 5
 /home/ddmallplaza/update/./xidel --proxy=127.0.0.1:8123 pasando.xml -f //imagen
+sleep 5
+rm *.xml
+sudo cp xml2json/$1/descuentos.json /home/ddmallplaza/mall/$2/json/descuentos.json
+sudo cp xml2json/$1/cine.json /home/ddmallplaza/mall/$2/json/cine.json
+sudo cp xml2json/$1/eventos.json /home/ddmallplaza/mall/$2/json/eventos.json
+sudo cp xml2json/$1/pasando.json /home/ddmallplaza/mall/$2/json/pasando.json
+pm2 reload all
 exit
