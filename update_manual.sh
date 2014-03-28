30cff5b41745721e4a4d093c2fed39a28bca7a20
d-sepulveda <d@splvd.com>
2014-03-28 13:35:31 -0300

    Update update_manual.sh

--- update_manual.sh
+++ update_manual.sh
@@ -2,14 +2,12 @@
 wget http://www.mallplaza.cl/xml/descuentos.php?siteid=mallplaza-$1 --output-document=descuentos.xml && sleep 10 && wget http://www.mallplaza.cl/xml/cine.php?siteid=mallplaza-$1 --output-document=cine.xml && sleep 10 && wget http://www.mallplaza.cl/xml/eventos.php?siteid=mallplaza-$1 --output-document=eventos.xml && sleep 10 && wget http://www.mallplaza.cl/xml/queestapasando.php?siteid=mallplaza-$1 --output-document=pasando.xml && sleep 10 && wget http://www.mallplaza.cl/xml/tiendas.php?siteid=mallplaza-$1 --output-document=tiendas.xml
 ls tiendas.xml >/dev/null || exit
 sudo squid3 -k shutdown
-sudo service squid3 stop
 sudo mv /home/ddmallplaza/squid_cache.old /home/ddmallplaza/squid_cache.borrar
 sudo mv /home/ddmallplaza/squid_cache /home/ddmallplaza/squid_cache.old
 mkdir /home/ddmallplaza/squid_cache
 sudo chown proxy:proxy /home/ddmallplaza/squid_cache
 sudo squid3 -z
-sudo squid3
-sudo service squid3 start
+sudo service squid3 restart
 sleep 5
 /home/ddmallplaza/update/./xidel --proxy=127.0.0.1:3128 descuentos.xml -f //imagen
 sleep 10
