a999d2f564e6bbea00602bb3a2525ff918fbf68f
d-sepulveda <d@splvd.com>
2014-05-19 11:56:57 -0400

    monitor apache

---
+++ monitor_apache.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+output=$(sudo /opt/lampp/./ctlscript.sh status apache)
+if [ "$output" == "apache already running" ]
+then
+	exit
+else
+	sudo rm /opt/lampp/logs/httpd.pid
+	sudo /opt/lampp/./lampp restart && sudo /opt/lampp/./lampp restart
+fi
