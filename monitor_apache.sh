c3f5cf4b160a58d284809642aa5a894f7392de94
d-sepulveda <d@splvd.com>
2014-06-04 12:32:09 -0400

    monitor apache

--- monitor_apache.sh
+++ monitor_apache.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 output=$(sudo /opt/lampp/./ctlscript.sh status apache)
-if [ "$output" == "apache already running" ]
-then
-	exit
+if [ "$output" == "apache already running" ]; then
+	echo -e "running"
 else
 	sudo rm /opt/lampp/logs/httpd.pid
 	sudo /opt/lampp/./lampp restart && sudo /opt/lampp/./lampp restart
---
+++ touch2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xinput set-prop "Touch Touch Device F55WH00U-CT-A1-4P" "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1
