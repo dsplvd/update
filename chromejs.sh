4d5cf02119212eb3bde66b30929db2542e364ba3
d-sepulveda <d@splvd.com>
2014-06-05 18:48:14 -0400

    Update chromejs.sh

--- chromejs.sh
+++ chromejs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export DISPLAY=:0.0
+xset -dpms
+xset s off
 xinput set-prop "FREE INTERACTIVE TECHNOLOGY " "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1
 openbox & google-chrome http://localhost:3000 --kiosk --incognito --proxy-server=127.0.0.1:3128 --disk-cache-dir=/dev/null
